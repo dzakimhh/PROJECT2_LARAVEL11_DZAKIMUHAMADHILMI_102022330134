1bfd1d6bbfb16ec5ddf6a54a28a49af11427149a
dzakimhh <dzakimh28@gmail.com>
2025-04-22 22:51:06 +0700

    Tugas Kedua

--- resources/views/websites.blade.php
+++ resources/views/websites.blade.php
@@ -4,13 +4,13 @@
     <title>Daftar Website Peminjaman PlayStation</title>
 </head>
 <body>
-    <h1>Daftar Website</h1>
+    <h1>Daftar Peminjam PlayStation</h1>
     <table border="1" cellpadding="10">
         <thead>
             <tr>
                 <th>ID</th>
                 <th>Nama</th>
-                <th>URL</th>
+                <th>Tanggal Meminjam</th>
                 <th>Deskripsi</th>
             </tr>
         </thead>
@@ -19,7 +19,7 @@
                 <tr>
                     <td>{{ $website->id }}</td>
                     <td>{{ $website->name }}</td>
-                    <td><a href="{{ $website->url }}" target="_blank">{{ $website->url }}</a></td>
+                    <td>{{ $website->tanggal_meminjam }}</td>
                     <td>{{ $website->description }}</td>
                 </tr>
             @endforeach
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebsiteController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/websites', [WebsiteController::class, 'index']);
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/', [WebsiteController::class, 'index']);
+Route::get('/websites', [WebsiteController::class, 'index'])->middleware('auth');
 
 Route::get('/', function () {
-    return redirect('websites');
-});
+    return redirect('login');
+})->middleware('auth');
